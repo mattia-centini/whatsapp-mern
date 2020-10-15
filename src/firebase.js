f88e5bbf8b4e3c2dbcc9fee1fbcf1812b6829fb9
Mattia Centini <mattia.centini@gmail.com>
2020-10-15 14:42:30 +0200

    setting up the chatbox and sidebar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8tIfUQmwKIpwXfYs9zHYnyOK6rQK3Caw",
+    authDomain: "whatsup-mern-32164.firebaseapp.com",
+    databaseURL: "https://whatsup-mern-32164.firebaseio.com",
+    projectId: "whatsup-mern-32164",
+    storageBucket: "whatsup-mern-32164.appspot.com",
+    messagingSenderId: "853653798681",
+    appId: "1:853653798681:web:2ef519411db9fd7f084f88"
+  };
