fc9ea3330d75c5ef407b21b42b85c267ac474bea
Mattia Centini <mattia.centini@gmail.com>
2020-10-15 22:19:33 +0200

    added button on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./Sidebar.css"
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import IconButton from '@material-ui/core/IconButton';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Im a sidebat
-                
-            </h1>
+            <h1>Im a sidebar</h1>
+             <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                          <DonutLargeIcon />
+                    </IconButton>
+                    
+                    <IconButton>
+                           <ChatIcon />
+                    </IconButton>
+                    
+                    
+                </div>      
+            </div>  
         </div>
     )
 }
